31fa97965c73bd09345499eb660790b0796ad894
Akanni Silva <akanni029@gmail.com>
2026-06-18 12:35:07 -0300

    deploy(app.module) -implementa servico do postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,14 +6,14 @@ import { ProductsModule } from './products/products.module';
 import { ScheduleModule } from './schedule/schedule.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
-import { DevService } from './auth/data/services/dev.service';
 import { AppController } from './app.controller';
+import { ProdService } from './auth/data/services/prod.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({
-      useClass: DevService,
+      useClass: ProdService,
       imports: [ConfigModule],
     }),
     AuthModule,

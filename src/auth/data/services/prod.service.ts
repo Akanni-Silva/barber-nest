7d02dfbc9cb5ea1bbe559f8a2950763cfec34397
Akanni Silva <akanni029@gmail.com>
2026-06-18 13:12:12 -0300

    deploy(pordService) -implementa servico do postgres

--- src/auth/data/services/prod.service.ts
+++ src/auth/data/services/prod.service.ts
@@ -5,25 +5,16 @@ import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
 @Injectable()
 export class ProdService implements TypeOrmOptionsFactory {
   createTypeOrmOptions(): TypeOrmModuleOptions {
-    const isProduction = process.env.NODE_ENV === 'production';
-
     return {
       type: 'postgres',
       url: process.env.DATABASE_URL,
-      logging: !isProduction,
+      logging: false,
       dropSchema: false,
       ssl: {
         rejectUnauthorized: false,
       },
-      synchronize: !isProduction,
+      synchronize: false,
       autoLoadEntities: true,
-      extra: {
-        max: 20,
-        connectionTimeoutMillis: 30000,
-        idleTimeoutMillis: 30000,
-      },
-      retryAttempts: 10,
-      retryDelay: 3000,
     };
   }
 }

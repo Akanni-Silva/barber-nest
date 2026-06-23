b9e05fa486277d434958c4e7fb2c128b5a646892
Akanni Silva <akanni029@gmail.com>
2026-06-23 14:55:39 -0300

    fix: adicionar Appointment no ScheduleModule

--- src/schedule/schedule.module.ts
+++ src/schedule/schedule.module.ts
@@ -6,6 +6,8 @@ import { WorkSchedule } from './entities/work-schedule.entity';
 import { BlockedDate } from './entities/blocked-date.entity';
 import { SpecialHours } from './entities/special-hours.entity';
 import { BreakTime } from './entities/break-time.entity';
+import { AppointmentsModule } from '../appointments/appointments.module';
+import { Appointment } from '../appointments/entities/appointment.entity';
 
 @Module({
   imports: [
@@ -14,7 +16,9 @@ import { BreakTime } from './entities/break-time.entity';
       BlockedDate,
       SpecialHours,
       BreakTime,
+      Appointment,
     ]),
+    AppointmentsModule,
   ],
   controllers: [ScheduleController],
   providers: [ScheduleService],
